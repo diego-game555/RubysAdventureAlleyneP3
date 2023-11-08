fda3ab4a4e897d5d46e3b766f7e3e4a754f50b21
Diego Alleyne <714289@student.dusd.net>
2023-11-08 11:40:42 -0800

    Fixed Colliders
    Fixed Ruby's, the metal box, and tiles colliders

--- P3RubysAdventureAlleyne/Assets/Scripts/RubyController.cs
+++ P3RubysAdventureAlleyne/Assets/Scripts/RubyController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -11,21 +16,29 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.Get.Axis("Vertical");
+        vertical = Input.GetAxis("Vertical");
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
