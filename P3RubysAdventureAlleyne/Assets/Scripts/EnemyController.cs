8aa99562ccd855a963804ebe769d6b5b79ea663d
Diego Alleyne <714289@student.dusd.net>
2023-11-19 17:45:05 -0800

    Finished Animation Scripts
    Finished coding the animation scripts for the robot and Ruby Controller.

--- P3RubysAdventureAlleyne/Assets/Scripts/EnemyController.cs
+++ P3RubysAdventureAlleyne/Assets/Scripts/EnemyController.cs
@@ -40,11 +40,15 @@ public class EnemyController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction;;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
         
 
